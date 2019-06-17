1972d61221c51283a85e34c0c5926daf46c5fab8
TipTopJT <51776116+TipTopJT@users.noreply.github.com>
2019-06-17 11:28:20 +0300

    comments

--- motor3.py
+++ motor3.py
@@ -7,13 +7,13 @@ pi.set_mode(21, pigpio.OUTPUT)
 
 #(steps) number of steps (direction) dirrection (0 or 1) one step 1,8 degree
 def P_motor(steps,direction):
-    pi.write(19, 0)  #enable
+    pi.write(19, 0)  #enable on
     pi.write(20, direction)
     pi.set_PWM_dutycycle(21, 128)
     pi.set_PWM_frequency(21, 600) #pulse frequency
     time.sleep(1/600*steps)
     pi.set_PWM_dutycycle(21, 0)
-    pi.write(19, 1) #enable
+    pi.write(19, 1) #enable off
 
 if __name__ == '__main__':
 	P_motor(100,1)  #(first) number of steps (second) dirrection (0 or 1) one step 1,8 degree
