88b42232613cb60b78cce4b3b5462b09fa935e5b
TipTopJT <51776116+TipTopJT@users.noreply.github.com>
2019-06-25 12:35:37 +0300

    FIX

--- i2c_RPM_FINAL/i2c_RPM_FINAL.ino
+++ i2c_RPM_FINAL/i2c_RPM_FINAL.ino
@@ -1,19 +1,20 @@
 #include <Wire.h>
-#include <avr/wdt.h>
+
 #define SLAVE_ADDRESS 0x08
 
 int buf= 70;
+int bufreal=buf-1;
 volatile unsigned long time[70];
 int tic=0;
-int RPM; 
+unsigned long RPM; 
 unsigned long t=0;
-
+int bufer_full=false;
 void setup()
 {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   pinMode(2, INPUT); 
   attachInterrupt(digitalPinToInterrupt(2), tickk, FALLING); 
-  wdt_enable(WDTO_8S); 
+
   Wire.begin(SLAVE_ADDRESS);
   Wire.onRequest(sendData);
  
@@ -21,16 +22,36 @@ void setup()
 
 void sendData()
 {
- if(tic<buf - 1)
- {
-   RPM=buf*60000/(millis()-time[tic+1]);
-   Serial.println(RPM);  
-  }
- if(tic==buf-1)
- {
-   RPM=buf*60000/(millis()-time[0]);
-   Serial.println(RPM);
- }   
+  if(!bufer_full )
+    {
+      if(tic>1)
+        {
+          RPM=(tic-1)*60000/(time[tic-1]-time[0]);
+        }
+      else
+        {
+          RPM=0;
+        }  
+    }
+  else
+    {
+     if(tic>0)
+      {
+        RPM=bufreal*60000/(time[tic-1]-time[tic]);
+      }
+     else
+      {
+       RPM=bufreal*60000/(time[bufreal]-time[0]); 
+      } 
+      
+    }
+   if(millis()-t>2000)
+    {
+      RPM=0;
+    }
+
+
+ 
  Wire.write((const uint8_t*)&RPM,4);
    
 }
@@ -39,15 +60,17 @@ void sendData()
 
 void loop()
 {
- wdt_reset();
+ 
 }
 
 void tickk() 
 { 
   time[tic]=millis();
+  t=time[tic];
   tic++; 
   if(tic>=buf) 
   {
+    bufer_full=true;
     tic=0;
   }
 }
